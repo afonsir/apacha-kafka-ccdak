1ae10c4f06ddead09165d05f15d841c7c776a33a
AfonSir <afonsir17@gmail.com>
2021-07-27 21:34:13 -0300

    fix: basic consumer

--- app/src/main/java/com/github/afonsir/basics/ConsumerMain.java
+++ app/src/main/java/com/github/afonsir/basics/ConsumerMain.java
@@ -2,6 +2,7 @@ package com.github.afonsir.basics;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
